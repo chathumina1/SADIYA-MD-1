3d703592ec25936c869881ba9f0eff96faa829d2
chathumina1 <chathuminasithum456@gmail.Com>
2025-05-05 11:42:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //Put Session-id Here
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "", //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "", //Put Your Github Auth Token
+SESSION_ID: process.env.SESSION_ID || "SADIYA-MD=rdcGBJ4S#r4DtKiH4F3ieamkS-CoMi9l3yd0wOwUM69SZX1YZGck", //Put Session-id Here
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "chathumina1", //Put Your Gitub Username
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "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", //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || "SADIYA-MD-DATABASE", //Bot Database Repo Name
 SESSION_NAME: process.env.SESSION_NAME || "session"
 };
